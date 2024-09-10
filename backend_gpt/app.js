f97dc9b2c3ca8491a17fe69089d4130d8ba89339
Alan Subedi <alansubedi@Alans-Laptop.local>
2024-09-10 15:44:05 -0500

    removed test routes and controllers from backend_gpt

--- backend_gpt/app.js
+++ backend_gpt/app.js
@@ -3,11 +3,9 @@ const express = require('express');
 const app = express();
 
 // Import routes
-const helloRoute = require('./routes/helloRoute');
 const gptRoute = require('./routes/gptRoute')
 
 // Use routes
-app.use('/', helloRoute);
 app.use('/api/v1/gpt-response', gptRoute);
 
 // Start the server
