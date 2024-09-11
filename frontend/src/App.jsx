1434073c122bebe5d273164dbe6e05467c6a24c7
Alan Subedi <alansubedi@Alans-Laptop.local>
2024-09-11 13:19:30 -0500

    router setup complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import Login from "./components/Login"
-
+import Iselp from "./components/Iselp";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 
   return (
-    <Login />
+    <Router>
+      <Routes>
+
+        <Route path="/" element={<Login />} />
+        <Route path="/iselp" element={<Iselp />} exact/>
+
+      </Routes>
+    </Router>
   )
 }
 
---
+++ frontend/src/components/Iselp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Iselp = () => {
+  return (
+    <h1>YOoooooooOOOOOOOOOOOO</h1>
+  )
+}
+
+export default Iselp
